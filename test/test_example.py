b465e86e7845736da613bee32b3d087d07c31adc
Joseph Curtis <jccurtis@lbl.gov>
2018-08-29 17:55:06 -0700

    Allow code package imports in test suite
    NOTE: this only works with nosetests called from the repository root directory

--- test/test_example.py
+++ test/test_example.py
@@ -1,5 +1,9 @@
 # Tests for the trivial example code in code/example.py
 from __future__ import absolute_import
+import sys
+import os
+# Temporarily add the repo path to the PYTHONPATH so that code can be imported
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from code.example import add
 
 from nose.tools import assert_raises
